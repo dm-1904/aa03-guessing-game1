f61f45570e472224062ca12ebe6607284a7deae2
damonryon <damon.ryon@gmail.com>
2024-04-17 20:52:01 -0700

    randomInt

--- guessing-game-project/guessing-game.js
+++ guessing-game-project/guessing-game.js
@@ -5,6 +5,12 @@ const rl = readline.createInterface({ input, output });
 
 let secretNumber = 5
 
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); 
+  }
+
 const checkGuess = (num) => {
     if(num > secretNumber) {
         console.log('Too High.')
